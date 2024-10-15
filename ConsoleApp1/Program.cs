480dee8c07cb3c07e07823b8ed2c3cbf6a0cb94d
David J. Straley <djstraley@sandyhook.com>
2024-10-15 12:08:13 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿string s;
+    
+s = "abcdba";
+
+if(mirror(s))
+{
+    Console.WriteLine($"{s} - Mirror");
+}
+else
+{
+    Console.WriteLine($"{s} - Not Mirror");
+}
+
+
+s = "abccba";
+if (mirror(s))
+{
+    Console.WriteLine($"{s} - Mirror");
+}
+else
+{
+    Console.WriteLine($"{s} - Not Mirror");
+}
+
+s = "abcba";
+if (mirror(s))
+{
+    Console.WriteLine($"{s} - Mirror");
+}
+else
+{
+    Console.WriteLine($"{s} - Not Mirror");
+}
+
+Boolean mirror(string s)
+{
+
+    Boolean bRetval = true;
+
+    int n = s.Length;
+
+    for (int i = 0, j = n - 1; i < n / 2; i++, j--)
+    {
+        if (s[i] == s[j])
+        {
+            continue;
+        }
+        else
+        {
+            bRetval = false;
+            break;
+        }
+    }
+
+    return (bRetval);
+}
